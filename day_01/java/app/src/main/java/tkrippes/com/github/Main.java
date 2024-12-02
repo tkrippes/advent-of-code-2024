4800e355b8ad8ca1cb15cab7c16054c51dcfe6af
tkrippes <tkrippes@live.de>
2024-12-02 21:32:49 +0100

    Implement solution of part 2
    add solution of part 2 to results

--- day_01/java/app/src/main/java/tkrippes/com/github/Main.java
+++ day_01/java/app/src/main/java/tkrippes/com/github/Main.java
@@ -2,6 +2,7 @@ package tkrippes.com.github;
 
 import tkrippes.com.github.parser.LocationIdListsParser;
 import tkrippes.com.github.solver.ListDistanceSolver;
+import tkrippes.com.github.solver.SimilarityScoreSolver;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,6 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Result of part 1: " + part1(inputFileName));
+        System.out.println("Result of part 2: " + part2(inputFileName));
     }
 
     private static int part1(String inputFileName) {
@@ -26,15 +28,40 @@ public class Main {
         }
 
         ListDistanceSolver solver = new ListDistanceSolver();
-        int solution;
+        int listDistance;
         try {
-            solution = solver.solve(locationIdLists.get(0), locationIdLists.get(1));
+            listDistance = solver.solve(locationIdLists.get(0), locationIdLists.get(1));
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
 
             return 0;
         }
 
-        return solution;
+        return listDistance;
+    }
+
+    private static int part2(String inputFileName) {
+        LocationIdListsParser parser = new LocationIdListsParser();
+
+        List<List<Integer>> locationIdLists;
+        try {
+            locationIdLists = parser.parse(inputFileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        SimilarityScoreSolver solver = new SimilarityScoreSolver();
+        int similarityScore;
+        try {
+            similarityScore = solver.solve(locationIdLists.get(0), locationIdLists.get(1));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        return similarityScore;
     }
 }
