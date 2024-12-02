e87ba7baaeeec5d6a442a0ed3308d0508bc662fb
tkrippes <tkrippes@live.de>
2024-12-02 15:06:51 +0100

    Fixed parsing logic
    delete unneeded variables

--- day_01/java/app/src/main/java/tkrippes/com/github/LocationIdListsParser.java
+++ day_01/java/app/src/main/java/tkrippes/com/github/LocationIdListsParser.java
@@ -2,6 +2,7 @@ package tkrippes.com.github;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LocationIdListsParser {
@@ -12,31 +13,27 @@ public class LocationIdListsParser {
             throw new FileNotFoundException("Could not find input file: " + inputFileName);
         }
 
-        List<List<Integer>> locationIdLists = new ArrayList<>();
-        List<Integer> firstLocationIdList = new ArrayList<>();
-        List<Integer> secondLocationIdList = new ArrayList<>();
+        List<List<Integer>> locationIdLists = new ArrayList<>(Arrays.asList(new ArrayList<>(), new ArrayList<>()));
 
-        // TODO fix logic
         try {
             FileReader fileReader = new FileReader(inputFile);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
 
             while ((line = bufferedReader.readLine()) != null) {
-                String[] tokens = line.split(" ");
+                String[] tokens = line.split("\\s+");
                 if (tokens.length != 2) {
+                    System.err.printf("Ignoring line, got %d instead of 2 tokens%n", tokens.length);
+
                     continue;
                 }
 
-                firstLocationIdList.add(Integer.parseInt(tokens[0]));
-                secondLocationIdList.add(Integer.parseInt(tokens[1]));
+                locationIdLists.get(0).add(Integer.parseInt(tokens[0]));
+                locationIdLists.get(1).add(Integer.parseInt(tokens[1]));
             }
         } catch (IOException e) {
             throw new IOException("Error reading file: " + inputFileName, e);
         }
-
-        locationIdLists.add(firstLocationIdList);
-        locationIdLists.add(secondLocationIdList);
 
         return locationIdLists;
     }
