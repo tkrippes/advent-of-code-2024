08d5b08ad232a979a6a66e226f7a76269db59c44
tkrippes <tkrippes@live.de>
2024-12-02 21:10:12 +0100

    Implement solution of part 1
    add solution of part 1 to results

--- day_01/java/app/src/main/java/tkrippes/com/github/Main.java
+++ day_01/java/app/src/main/java/tkrippes/com/github/Main.java
@@ -1,5 +1,8 @@
 package tkrippes.com.github;
 
+import tkrippes.com.github.parser.LocationIdListsParser;
+import tkrippes.com.github.solver.ListDistanceSolver;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -7,11 +10,31 @@ public class Main {
     private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
+        System.out.println("Result of part 1: " + part1(inputFileName));
+    }
+
+    private static int part1(String inputFileName) {
         LocationIdListsParser parser = new LocationIdListsParser();
+
+        List<List<Integer>> locationIdLists;
         try {
-            List<List<Integer>> locationIdLists = parser.parse(inputFileName);
+            locationIdLists = parser.parse(inputFileName);
         } catch (IOException e) {
             e.printStackTrace();
+
+            return 0;
         }
+
+        ListDistanceSolver solver = new ListDistanceSolver();
+        int solution;
+        try {
+            solution = solver.solve(locationIdLists.get(0), locationIdLists.get(1));
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        return solution;
     }
 }
