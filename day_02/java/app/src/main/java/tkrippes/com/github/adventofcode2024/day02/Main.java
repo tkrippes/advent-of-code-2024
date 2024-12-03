5d4d2ab0119a976ae28050f94a9367d5f55866fa
Tom Krippes <tom.krippes@weinig.com>
2024-12-03 13:49:40 +0100

    Add part 1 to main
    remove unneeded test file

--- day_02/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day02/Main.java
+++ day_02/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day02/Main.java
@@ -1,11 +1,40 @@
 package tkrippes.com.github.adventofcode2024.day02;
 
+import tkrippes.com.github.adventofcode2024.day02.parser.ReportListParser;
+import tkrippes.com.github.adventofcode2024.day02.solver.ReportSafetyCountSolver;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        System.out.println("Result of part 1: " + part1(inputFileName));
+    }
+
+    private static int part1(String inputFileName) {
+        ReportListParser parser = new ReportListParser();
+
+        List<List<Integer>> reportList;
+        try {
+            reportList = parser.parse(inputFileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        ReportSafetyCountSolver solver = new ReportSafetyCountSolver();
+        int numberOfSafeReports;
+        try {
+            numberOfSafeReports = solver.solve(reportList);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        return numberOfSafeReports;
     }
 }
