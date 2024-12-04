612be252d85f7cf5d16a9ec3456ed87c278c8450
tkrippes <tkrippes@live.de>
2024-12-04 21:45:01 +0100

    Added position index to multiplications parser
    Adapt reader to read all input at once
    Adapt tests

--- day_03/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day03/parser/MemoryParser.java
+++ day_03/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day03/parser/MemoryParser.java
@@ -3,10 +3,11 @@ package tkrippes.com.github.adventofcode2024.day03.parser;
 import tkrippes.com.github.adventofcode2024.day03.Memory;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class MemoryParser {
     public Memory parse(String inputFileName) throws IOException {
@@ -15,33 +16,30 @@ public class MemoryParser {
             throw new FileNotFoundException("Could not find input file: " + inputFileName);
         }
 
-        List<Memory.Multiplication> multiplications = new ArrayList<>();
+        Map<Integer, Memory.Multiplication> multiplications;
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                multiplications.addAll(parseMultiplications(line));
-            }
-
+            multiplications = parseMultiplications(reader.lines().collect(Collectors.joining()));
             reader.close();
         } catch (IOException e) {
             throw new IOException("Error reading file: " + inputFile, e);
         }
 
-        return new Memory(multiplications);
+        return new Memory(multiplications.values().stream().toList());
     }
 
-    protected List<Memory.Multiplication> parseMultiplications(String input) {
-        List<Memory.Multiplication> multiplications = new ArrayList<>();
+    protected Map<Integer, Memory.Multiplication> parseMultiplications(String input) {
+        // LinkedHashMap needed to preserve insertion order
+        Map<Integer, Memory.Multiplication> multiplications = new LinkedHashMap<>();
 
         String regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
         while (matcher.find()) {
-            multiplications.add(new Memory.Multiplication(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2))));
+            multiplications.put(matcher.start(), new Memory.Multiplication(Integer.parseInt(matcher.group(1)),
+                    Integer.parseInt(matcher.group(2))));
         }
 
         return multiplications;
--- day_03/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day03/parser/MemoryParserTest.java
+++ day_03/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day03/parser/MemoryParserTest.java
@@ -8,6 +8,7 @@ import tkrippes.com.github.adventofcode2024.day03.Memory;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -27,37 +28,39 @@ public class MemoryParserTest {
 
     @Test
     public void parserParsesValidSingleDigitMultiplicationCorrectly() {
-        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3,6)");
-        assertEquals(List.of(new Memory.Multiplication(3, 6)), parsedMultiplications);
+        Map<Integer, Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3,6)");
+        assertEquals(Map.of(0, new Memory.Multiplication(3, 6)), parsedMultiplications);
     }
 
     @Test
     public void parserParsesValidDoubleDigitMultiplicationCorrectly() {
-        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(33,66)");
-        assertEquals(List.of(new Memory.Multiplication(33, 66)), parsedMultiplications);
+        Map<Integer, Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(33,66)");
+        assertEquals(Map.of(0, new Memory.Multiplication(33, 66)), parsedMultiplications);
     }
 
     @Test
     public void parserParsesValidTripleDigitMultiplicationCorrectly() {
-        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(333,666)");
-        assertEquals(List.of(new Memory.Multiplication(333, 666)), parsedMultiplications);
+        Map<Integer, Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(333,666)");
+        assertEquals(Map.of(0, new Memory.Multiplication(333, 666)), parsedMultiplications);
     }
 
     @Test
     public void parserDoesNotParseValidQuadrupleDigitMultiplication() {
-        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3333,66666)");
+        Map<Integer, Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3333,66666)");
         assertTrue(parsedMultiplications.isEmpty());
     }
 
     @Test
     public void parserParsesValidMultipleMultiplicationCorrectly() {
-        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3,666)mul(33,6)mul(333,66)");
-        assertEquals(List.of(new Memory.Multiplication(3, 666), new Memory.Multiplication(33, 6), new Memory.Multiplication(333, 66)), parsedMultiplications);
+        Map<Integer, Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3,666)mul(33,6)" +
+                "mul(333,66)");
+        assertEquals(Map.of(0, new Memory.Multiplication(3, 666), 10, new Memory.Multiplication(33, 6),
+                19, new Memory.Multiplication(333, 66)), parsedMultiplications);
     }
 
     @Test
     public void parserDoesNotParseInvalidMultiplications() {
-        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(4*");
+        Map<Integer, Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(4*");
         assertTrue(parsedMultiplications.isEmpty());
         parsedMultiplications = parser.parseMultiplications("mul(6,9!");
         assertTrue(parsedMultiplications.isEmpty());
