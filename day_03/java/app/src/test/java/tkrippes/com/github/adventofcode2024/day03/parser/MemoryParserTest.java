1a5a3068281810b6a66d5b11d0a8c0fb6d5d6e60
Tom Krippes <tom.krippes@weinig.com>
2024-12-04 14:35:13 +0100

    Add more multiplication parsing tests

--- day_03/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day03/parser/MemoryParserTest.java
+++ day_03/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day03/parser/MemoryParserTest.java
@@ -26,9 +26,45 @@ public class MemoryParserTest {
     }
 
     @Test
-    public void parserParsesValidSingleMultiplicationCorrectly() {
+    public void parserParsesValidSingleDigitMultiplicationCorrectly() {
         List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3,6)");
         assertEquals(List.of(new Memory.Multiplication(3, 6)), parsedMultiplications);
+    }
+
+    @Test
+    public void parserParsesValidDoubleDigitMultiplicationCorrectly() {
+        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(33,66)");
+        assertEquals(List.of(new Memory.Multiplication(33, 66)), parsedMultiplications);
+    }
+
+    @Test
+    public void parserParsesValidTripleDigitMultiplicationCorrectly() {
+        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(333,666)");
+        assertEquals(List.of(new Memory.Multiplication(333, 666)), parsedMultiplications);
+    }
+
+    @Test
+    public void parserDoesNotParseValidQuadrupleDigitMultiplication() {
+        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3333,66666)");
+        assertTrue(parsedMultiplications.isEmpty());
+    }
+
+    @Test
+    public void parserParsesValidMultipleMultiplicationCorrectly() {
+        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(3,666)mul(33,6)mul(333,66)");
+        assertEquals(List.of(new Memory.Multiplication(3, 666), new Memory.Multiplication(33, 6), new Memory.Multiplication(333, 66)), parsedMultiplications);
+    }
+
+    @Test
+    public void parserDoesNotParseInvalidMultiplications() {
+        List<Memory.Multiplication> parsedMultiplications = parser.parseMultiplications("mul(4*");
+        assertTrue(parsedMultiplications.isEmpty());
+        parsedMultiplications = parser.parseMultiplications("mul(6,9!");
+        assertTrue(parsedMultiplications.isEmpty());
+        parsedMultiplications = parser.parseMultiplications("?(12,34)");
+        assertTrue(parsedMultiplications.isEmpty());
+        parsedMultiplications = parser.parseMultiplications("mul ( 2 , 4 )");
+        assertTrue(parsedMultiplications.isEmpty());
     }
 
     @After
