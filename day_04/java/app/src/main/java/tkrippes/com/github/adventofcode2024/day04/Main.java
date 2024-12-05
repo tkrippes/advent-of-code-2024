dfa2dfcfb5e6d51df0dab42bacddb530a3b95b3a
tkrippes <tkrippes@live.de>
2024-12-05 21:05:44 +0100

    Create gradle template for day 4
    rename package
    rename app to main

---
+++ day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/Main.java
@@ -0,0 +1,11 @@
+package tkrippes.com.github.adventofcode2024.day04;
+
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
