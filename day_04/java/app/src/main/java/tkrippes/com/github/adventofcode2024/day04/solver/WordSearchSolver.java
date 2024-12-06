ff7de6f7b82e395ceaa9919cb43536125415294c
Tom Krippes <tom.krippes@weinig.com>
2024-12-06 13:57:35 +0100

    Add WordSearchSolver
    implement logic to count words in normal direction
    add tests

---
+++ day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/solver/WordSearchSolver.java
@@ -0,0 +1,29 @@
+package tkrippes.com.github.adventofcode2024.day04.solver;
+
+import tkrippes.com.github.adventofcode2024.day04.WordSearch;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WordSearchSolver {
+    public int solve(WordSearch wordSearch, String word) {
+        if (word.isEmpty()) {
+            throw new IllegalArgumentException("Cannot search for empty word");
+        }
+
+        if (wordSearch.isEmpty()) {
+            return 0;
+        }
+
+        int wordCount = 0;
+        Pattern pattern = Pattern.compile(word);
+        for (int i = 0; i < wordSearch.height(); i++) {
+            Matcher matcher = pattern.matcher(wordSearch.getLine(i));
+            while (matcher.find()) {
+                wordCount++;
+            }
+        }
+
+        return wordCount;
+    }
+}
---
+++ day_04/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day04/solver/WordSearchSolverTest.java
@@ -0,0 +1,82 @@
+package tkrippes.com.github.adventofcode2024.day04.solver;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import tkrippes.com.github.adventofcode2024.day04.WordSearch;
+
+import static org.junit.Assert.*;
+
+public class WordSearchSolverTest {
+    private WordSearchSolver solver;
+    private WordSearch wordSearch;
+
+    @Before
+    public void setUp() {
+        solver = new WordSearchSolver();
+        wordSearch = new WordSearch();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void solveThrowsOnEmptyWord() {
+        solver.solve(wordSearch, "");
+    }
+
+    @Test
+    public void solveShouldReturnZeroOnEmptyWordSearch() {
+        assertEquals(0, solver.solve(wordSearch, "Test"));
+    }
+
+    @Test
+    public void solveShouldReturnZeroWhenWordDoesNotExistInWordSearch() {
+        wordSearch.add("Hello");
+        assertEquals(0, solver.solve(wordSearch, "Test"));
+    }
+
+    @Test
+    public void solveShouldReturnOneWhenWordSearchAndWordMatch() {
+        wordSearch.add("Test");
+        assertEquals(1, solver.solve(wordSearch, "Test"));
+    }
+
+    @Test
+    public void solveShouldReturnThreeWhenWordSearchAndWordMatchForThreeLines() {
+        wordSearch.add("Test");
+        wordSearch.add("Test");
+        wordSearch.add("Test");
+        assertEquals(3, solver.solve(wordSearch, "Test"));
+    }
+
+    @Test
+    public void solveShouldReturnOneWhenWordSearchContainsWord() {
+        wordSearch.add("AbTestDef");
+        assertEquals(1, solver.solve(wordSearch, "Test"));
+    }
+
+    @Test
+    public void solveShouldReturnThreeWhenWordIsContainedInTheWordSearchLines() {
+        wordSearch.add("TestTe");
+        wordSearch.add("ABTest");
+        wordSearch.add("TestCD");
+        assertEquals(3, solver.solve(wordSearch, "Test"));
+    }
+
+    @Test
+    public void solveShouldReturnTwoIfWordIsContainTwiceInOneLine() {
+        wordSearch.add("TestTest");
+        assertEquals(2, solver.solve(wordSearch, "Test"));
+    }
+
+    @Test
+    public void solveCountCorrectlyIfWordIsContainMultipleTimesInMultipleLines() {
+        wordSearch.add("AbcdTestEfgh");
+        wordSearch.add("TestTestEfgh");
+        wordSearch.add("TestTestTest");
+        wordSearch.add("AbcdEfghIjkl");
+        assertEquals(6, solver.solve(wordSearch, "Test"));
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
