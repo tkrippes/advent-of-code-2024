e93b167d01f142c05bdd4d602d9c7946a11c6fd2
Tom Krippes <tom.krippes@weinig.com>
2024-12-06 13:02:41 +0100

    Use WordSearch for WordSearch parser
    adapt functional code
    adapt tests
    make protected methods default

--- day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/parser/WordSearchParser.java
+++ day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/parser/WordSearchParser.java
@@ -1,18 +1,18 @@
 package tkrippes.com.github.adventofcode2024.day04.parser;
 
+import tkrippes.com.github.adventofcode2024.day04.WordSearch;
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class WordSearchParser {
-    public List<List<Character>> parse(String inputFileName) throws IOException {
+    public WordSearch parse(String inputFileName) throws IOException {
         File inputFile = new File(inputFileName);
         if (!inputFile.exists()) {
             throw new FileNotFoundException("Could not find input file: " + inputFileName);
         }
 
-        List<List<Character>> wordSearch;
+        WordSearch wordSearch;
         try {
             BufferedReader reader = new BufferedReader(new FileReader(inputFile));
             wordSearch = parseWordSearch(reader.lines().collect(Collectors.joining("\n")));
@@ -24,12 +24,12 @@ public class WordSearchParser {
         return wordSearch;
     }
 
-    protected List<List<Character>> parseWordSearch(String input) {
+    WordSearch parseWordSearch(String input) {
         if (input.isEmpty()) {
-            return new ArrayList<>();
+            return new WordSearch();
         }
 
-        List<List<Character>> wordSearch = new ArrayList<>();
+        WordSearch wordSearch = new WordSearch();
         for (String inputLine : input.lines().toList()) {
             if (doLineLengthDiffer(inputLine, wordSearch)) {
                 throw new IllegalArgumentException("All input lines need to have the same length");
@@ -41,7 +41,7 @@ public class WordSearchParser {
         return wordSearch;
     }
 
-    private boolean doLineLengthDiffer(String inputLine, List<List<Character>> wordSearch) {
-        return !wordSearch.isEmpty() && inputLine.length() != wordSearch.getLast().size();
+    private boolean doLineLengthDiffer(String inputLine, WordSearch wordSearch) {
+        return !wordSearch.isEmpty() && inputLine.length() != wordSearch.width();
     }
 }
--- day_04/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day04/parser/WordSearchParserTest.java
+++ day_04/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day04/parser/WordSearchParserTest.java
@@ -1,5 +1,7 @@
 package tkrippes.com.github.adventofcode2024.day04.parser;
 
+import tkrippes.com.github.adventofcode2024.day04.WordSearch;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -26,23 +28,25 @@ public class WordSearchParserTest {
 
     @Test
     public void parseEmptyWordSearchShouldReturnEmptyResult() {
-        List<List<Character>> parsedWordSearch = parser.parseWordSearch("");
+        WordSearch parsedWordSearch = parser.parseWordSearch("");
         assertTrue(parsedWordSearch.isEmpty());
     }
 
     @Test
     public void parseOneLineWordSearchCorrectly() {
-        List<List<Character>> parsedWordSearch = parser.parseWordSearch("FASDFKFASD");
-        assertEquals(List.of(List.of('F', 'A', 'S', 'D', 'F', 'K', 'F', 'A', 'S', 'D')), parsedWordSearch);
+        WordSearch parsedWordSearch = parser.parseWordSearch("FASDFKFASD");
+        WordSearch expectedWordSearch = new WordSearch();
+        expectedWordSearch.add(List.of('F', 'A', 'S', 'D', 'F', 'K', 'F', 'A', 'S', 'D'));
+        assertEquals(expectedWordSearch, parsedWordSearch);
     }
 
     @Test
     public void parseTwoLineSameLengthWordSearchCorrectly() {
-        List<List<Character>> parsedWordSearch = parser.parseWordSearch("FASDFKFASD\nFGDLJDSFMX");
-        assertEquals(List.of(
-                        List.of('F', 'A', 'S', 'D', 'F', 'K', 'F', 'A', 'S', 'D'),
-                        List.of('F', 'G', 'D', 'L', 'J', 'D', 'S', 'F', 'M', 'X')),
-                parsedWordSearch);
+        WordSearch parsedWordSearch = parser.parseWordSearch("FASDFKFASD\nFGDLJDSFMX");
+        WordSearch expectedWordSearch = new WordSearch();
+        expectedWordSearch.add(List.of('F', 'A', 'S', 'D', 'F', 'K', 'F', 'A', 'S', 'D'));
+        expectedWordSearch.add(List.of('F', 'G', 'D', 'L', 'J', 'D', 'S', 'F', 'M', 'X'));
+        assertEquals(expectedWordSearch, parsedWordSearch);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -52,12 +56,12 @@ public class WordSearchParserTest {
 
     @Test
     public void parseThreeLineSameLengthWordSearchCorrectly() {
-        List<List<Character>> parsedWordSearch = parser.parseWordSearch("FASDFKFASD\nFGDLJDSFMX\nAFSKLFASDJ");
-        assertEquals(List.of(
-                        List.of('F', 'A', 'S', 'D', 'F', 'K', 'F', 'A', 'S', 'D'),
-                        List.of('F', 'G', 'D', 'L', 'J', 'D', 'S', 'F', 'M', 'X'),
-                        List.of('A', 'F', 'S', 'K', 'L', 'F', 'A', 'S', 'D', 'J')),
-                parsedWordSearch);
+        WordSearch parsedWordSearch = parser.parseWordSearch("FASDFKFASD\nFGDLJDSFMX\nAFSKLFASDJ");
+        WordSearch expectedWordSearch = new WordSearch();
+        expectedWordSearch.add(List.of('F', 'A', 'S', 'D', 'F', 'K', 'F', 'A', 'S', 'D'));
+        expectedWordSearch.add(List.of('F', 'G', 'D', 'L', 'J', 'D', 'S', 'F', 'M', 'X'));
+        expectedWordSearch.add(List.of('A', 'F', 'S', 'K', 'L', 'F', 'A', 'S', 'D', 'J'));
+        assertEquals(expectedWordSearch, parsedWordSearch);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -73,19 +77,21 @@ public class WordSearchParserTest {
     @Test
     public void parseWordSearchTestInputFileCorrectly() throws IOException {
         String validInputFileName = "../../input/test_input.txt";
-        List<List<Character>> parsedWordSearch = parser.parse(validInputFileName);
-        assertEquals(List.of(
-                        List.of('M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'),
-                        List.of('M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'),
-                        List.of('A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'),
-                        List.of('M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'),
-                        List.of('X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'),
-                        List.of('X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'),
-                        List.of('S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'),
-                        List.of('S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'),
-                        List.of('M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'),
-                        List.of('M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X')),
-                parsedWordSearch);
+        WordSearch parsedWordSearch = parser.parse(validInputFileName);
+
+        WordSearch expectedWordSearch = new WordSearch();
+        expectedWordSearch.add(List.of('M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'));
+        expectedWordSearch.add(List.of('M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'));
+        expectedWordSearch.add(List.of('A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'));
+        expectedWordSearch.add(List.of('M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'));
+        expectedWordSearch.add(List.of('X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'));
+        expectedWordSearch.add(List.of('X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'));
+        expectedWordSearch.add(List.of('S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'));
+        expectedWordSearch.add(List.of('S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'));
+        expectedWordSearch.add(List.of('M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'));
+        expectedWordSearch.add(List.of('M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X'));
+
+        assertEquals(expectedWordSearch, parsedWordSearch);
     }
 
     @After
