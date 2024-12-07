a742f3ec21bb5f0d79611694785cd347674482bf
tkrippes <tkrippes@live.de>
2024-12-07 22:28:53 +0100

    Add main for part 1 of day 4
    add result for part 1 of day 4
    remove unneeded main test

--- day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/Main.java
+++ day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/Main.java
@@ -1,11 +1,35 @@
 package tkrippes.com.github.adventofcode2024.day04;
 
+import tkrippes.com.github.adventofcode2024.day04.parser.WordSearchParser;
+import tkrippes.com.github.adventofcode2024.day04.solver.WordSearchSolver;
+
+import java.io.IOException;
+
 public class Main {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final String inputFileName = "../../input/input.txt";
+    private static final String wordToCount = "XMAS";
 
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        System.out.println("Result of part 1: " + part1());
+        System.out.println("Result of part 2: " + part2());
+    }
+
+    private static int part1() {
+        WordSearchParser parser = new WordSearchParser();
+        WordSearch wordSearch;
+        try {
+            wordSearch = parser.parse(inputFileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        return new WordSearchSolver().solve(wordSearch, wordToCount);
+    }
+
+    private static int part2() {
+        // TODO
+        return 0;
     }
 }
--- day_04/results.md
+++ day_04/results.md
@@ -2,7 +2,7 @@
 
 ## Part 1
 
-TBD
+2549
 
 ## Part 2
 
