3705fd4f9114f13d5362e114e34dd87f6824db35
tkrippes <tkrippes@live.de>
2024-12-09 22:10:16 +0100

    Add method for second part
    WIP
    add test
    add todos for next steps

--- day_04/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day04/solver/WordSearchSolverTest.java
+++ day_04/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day04/solver/WordSearchSolverTest.java
@@ -200,6 +200,21 @@ public class WordSearchSolverTest {
         assertEquals(18, solver.solve(wordSearch, "XMAS"));
     }
 
+    @Test
+    public void solveCrossedWordTestInputFileContentCorrectly() {
+        wordSearch.add("MMMSXXMASM");
+        wordSearch.add("MSAMXMSMSA");
+        wordSearch.add("AMXSXMAAMM");
+        wordSearch.add("MSAMASMSMX");
+        wordSearch.add("XMASAMXAMM");
+        wordSearch.add("XXAMMXXAMA");
+        wordSearch.add("SMSMSASXSS");
+        wordSearch.add("SAXAMASAAA");
+        wordSearch.add("MAMMMXMMMM");
+        wordSearch.add("MXMXAXMASX");
+        assertEquals(9, solver.solveCrossedWord(wordSearch, "MAS"));
+    }
+
     @After
     public void tearDown() {
     }
