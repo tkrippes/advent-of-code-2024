e57831dd9468e1f65a9394260a00bfaaef26d022
Tom Krippes <tom.krippes@weinig.com>
2024-12-10 09:55:38 +0100

    Add solution for part 2 to main
    rework part 1 of main

--- day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/Main.java
+++ day_04/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day04/Main.java
@@ -1,20 +1,21 @@
 package tkrippes.com.github.adventofcode2024.day04;
 
 import tkrippes.com.github.adventofcode2024.day04.parser.WordSearchParser;
+import tkrippes.com.github.adventofcode2024.day04.solver.WordSearchCrossSolver;
 import tkrippes.com.github.adventofcode2024.day04.solver.WordSearchSolver;
+import tkrippes.com.github.adventofcode2024.day04.solver.WordSearchStraightSolver;
 
 import java.io.IOException;
 
 public class Main {
     private static final String inputFileName = "../../input/input.txt";
-    private static final String wordToCount = "XMAS";
 
     public static void main(String[] args) {
-        System.out.println("Result of part 1: " + part1());
-        System.out.println("Result of part 2: " + part2());
+        System.out.println("Result of part 1: " + commonPart(new WordSearchStraightSolver(), "XMAS"));
+        System.out.println("Result of part 2: " + commonPart(new WordSearchCrossSolver(), "MAS"));
     }
 
-    private static int part1() {
+    private static int commonPart(WordSearchSolver solver, String word) {
         WordSearchParser parser = new WordSearchParser();
         WordSearch wordSearch;
         try {
@@ -25,11 +26,6 @@ public class Main {
             return 0;
         }
 
-        return new WordSearchSolver().solve(wordSearch, wordToCount);
-    }
-
-    private static int part2() {
-        // TODO
-        return 0;
+        return solver.solve(wordSearch, word);
     }
 }
