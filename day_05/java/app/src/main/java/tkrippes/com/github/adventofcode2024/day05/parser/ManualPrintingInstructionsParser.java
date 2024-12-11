9f0e6c90ff06ba6f2fd28a72a558c639fc8bae0b
Tom Krippes <tom.krippes@weinig.com>
2024-12-11 11:48:48 +0100

    Implemented parse and parseManualPrintingInstructions
    add tests
    rename tests

--- day_05/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day05/parser/ManualPrintingInstructionsParser.java
+++ day_05/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day05/parser/ManualPrintingInstructionsParser.java
@@ -2,12 +2,57 @@ package tkrippes.com.github.adventofcode2024.day05.parser;
 
 import tkrippes.com.github.adventofcode2024.day05.ManualPrintingInstructions;
 
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class ManualPrintingInstructionsParser {
-    public ManualPrintingInstructions parse(String inputFileName) throws FileNotFoundException {
-        throw new FileNotFoundException("Could not find input file: " + inputFileName);
+    public ManualPrintingInstructions parse(String inputFileName) throws IOException {
+        File inputFile = new File(inputFileName);
+        if (!inputFile.exists()) {
+            throw new FileNotFoundException("Could not find input file: " + inputFileName);
+        }
+
+        ManualPrintingInstructions manualPrintingInstructions;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+
+            manualPrintingInstructions = parseManualPrintingInstructions(reader.lines().toList());
+
+            reader.close();
+        } catch (IOException e) {
+            throw new IOException("Error reading file: " + inputFile, e);
+        }
+
+        return manualPrintingInstructions;
+    }
+
+    ManualPrintingInstructions parseManualPrintingInstructions(List<String> inputs) {
+        if (!inputs.contains("")) {
+            throw new IllegalArgumentException("Inputs do not contain an empty line to separate rules and pages input");
+        }
+
+        List<String> rulesInput = inputs.subList(0, inputs.indexOf(""));
+        if (rulesInput.isEmpty()) {
+            throw new IllegalArgumentException("Inputs do not contain any rules");
+        }
+
+        List<ManualPrintingInstructions.PageOrderingRule> rules = rulesInput
+                .stream()
+                .map(this::parsePageOrderingRule)
+                .toList();
+
+        List<String> pagesInput = inputs.subList(inputs.indexOf("") + 1, inputs.size());
+        if (pagesInput.isEmpty()) {
+            throw new IllegalArgumentException("Inputs do not contain any pages");
+        }
+
+        List<ManualPrintingInstructions.PagesToProduce> pages = pagesInput
+                .stream()
+                .map(this::parsePagesToProduce)
+                .toList();
+
+        return new ManualPrintingInstructions(rules, pages);
     }
 
     ManualPrintingInstructions.PageOrderingRule parsePageOrderingRule(String input) {
--- day_05/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day05/parser/ManualPrintingInstructionsParserTest.java
+++ day_05/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day05/parser/ManualPrintingInstructionsParserTest.java
@@ -21,106 +21,148 @@ public class ManualPrintingInstructionsParserTest {
     }
 
     @Test
+    public void parseManualPrintingInstructionsThrowsOnEmptyInputs() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parseManualPrintingInstructions(List.of()));
+    }
+
+    @Test
+    public void parseManualPrintingInstructionsThrowsOnMissingEmptyLine() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parseManualPrintingInstructions(List.of("25|26", "25,26")));
+    }
+
+    @Test
+    public void parseManualPrintingInstructionsThrowsOnMissingRules() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parseManualPrintingInstructions(List.of("", "25,26")));
+    }
+
+    @Test
+    public void parseManualPrintingInstructionsThrowsOnMissingPages() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parseManualPrintingInstructions(List.of("25|26", "")));
+    }
+
+    @Test
+    public void parseManualPrintingInstructionsReturnsManualCorrectly() {
+        List<ManualPrintingInstructions.PageOrderingRule> expectedRules = List.of(
+                new ManualPrintingInstructions.PageOrderingRule(25, 26),
+                new ManualPrintingInstructions.PageOrderingRule(26, 27),
+                new ManualPrintingInstructions.PageOrderingRule(27, 28),
+                new ManualPrintingInstructions.PageOrderingRule(28, 29),
+                new ManualPrintingInstructions.PageOrderingRule(29, 30));
+
+        List<ManualPrintingInstructions.PagesToProduce> expectedPages = List.of(
+                new ManualPrintingInstructions.PagesToProduce(List.of(25, 26, 27)),
+                new ManualPrintingInstructions.PagesToProduce(List.of(25, 27, 29)),
+                new ManualPrintingInstructions.PagesToProduce(List.of(30, 28, 26)),
+                new ManualPrintingInstructions.PagesToProduce(List.of(25, 28, 27, 30, 29, 26)));
+
+        assertEquals(new ManualPrintingInstructions(expectedRules, expectedPages),
+                parser.parseManualPrintingInstructions(List.of("25|26", "26|27", "27|28", "28|29", "29|30", "",
+                        "25,26,27", "25,27,29", "30,28,26", "25,28,27,30,29,26")));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnEmptyRule() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule(""));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnSingleNumberRule() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("25"));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnMissingNumbersRule() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("|"));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnMissingFirstNumberRule() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("|26"));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnMissingSecondNumberRule() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("25|"));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnNonNumbersRule() {
+        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePageOrderingRule("AB|CD"));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnThreeNumbersRule() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("25|26|27"));
+    }
+
+    @Test
+    public void parsePageOrderingRuleThrowsOnTwoNumbersIncludingSpacesRule() {
+        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePageOrderingRule("25 | 26"));
+    }
+
+    @Test
+    public void parsePageOrderingRuleTwoNumbersRuleCalculatesRuleCorrectly() {
+        assertEquals(new ManualPrintingInstructions.PageOrderingRule(25, 26),
+                parser.parsePageOrderingRule("25|26"));
+    }
+
+    @Test
+    public void parsePagesToProduceThrowsOnEmptyPages() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePagesToProduce(""));
+    }
+
+    @Test
+    public void parsePagesToProduceThrowsOnMissingNumbersPages() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePagesToProduce(","));
+    }
+
+    @Test
+    public void parsePagesToProduceThrowsOnMissingFirstNumberPages() {
+        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePagesToProduce(",26"));
+    }
+
+    @Test
+    public void parsePagesToProduceThrowsOnNonNumbersPages() {
+        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePagesToProduce("AB,CD"));
+    }
+
+    @Test
+    public void parsePagesToProduceThrowsOnTwoNumbersIncludingSpacesPages() {
+        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePagesToProduce("25, 26"));
+    }
+
+    @Test
+    public void parsePagesToProduceSingleNumberPagesReturnsCorrectly() {
+        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(25)),
+                parser.parsePagesToProduce("25"));
+    }
+
+    @Test
+    public void parsePagesToProduceTwoNumbersPagesReturnsCorrectly() {
+        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(25, 26)),
+                parser.parsePagesToProduce("25,26"));
+    }
+
+    @Test
+    public void parsePagesToProduceOmitsEmptyNumberPagesToProduceAtTheEnd() {
+        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(25, 26)),
+                parser.parsePagesToProduce("25,26,"));
+    }
+
+    @Test
+    public void parsePagesToProduceTenNumbersPagesReturnCorrectly() {
+        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
+                parser.parsePagesToProduce("1,2,3,4,5,6,7,8,9,10"));
+    }
+
+    @Test
     public void parseThrowsForInvalidInputFileName() {
         String invalidInputFileName = "../../input/invalid_test_input.txt";
 
         Assertions.assertThrows(FileNotFoundException.class, () -> parser.parse(invalidInputFileName));
     }
 
-    @Test
-    public void parseThrowsOnEmptyPageOrderingRule() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule(""));
-    }
-
-    @Test
-    public void parseThrowsOnSingleNumberPageOrderingRule() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("25"));
-    }
-
-    @Test
-    public void parseThrowsOnMissingNumbersPageOrderingRule() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("|"));
-    }
-
-    @Test
-    public void parseThrowsOnMissingFirstNumberPageOrderingRule() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("|26"));
-    }
-
-    @Test
-    public void parseThrowsOnMissingSecondNumberPageOrderingRule() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("25|"));
-    }
-
-    @Test
-    public void parseThrowsOnNonNumbersPageOrderingRule() {
-        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePageOrderingRule("AB|CD"));
-    }
-
-    @Test
-    public void parseThrowsOnThreeNumbersPageOrderingRule() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePageOrderingRule("25|26|27"));
-    }
-
-    @Test
-    public void parseThrowsOnTwoNumbersIncludingSpacesPageOrderingRule() {
-        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePageOrderingRule("25 | 26"));
-    }
-
-    @Test
-    public void parseTwoNumbersPageOrderingRule() {
-        assertEquals(new ManualPrintingInstructions.PageOrderingRule(25, 26),
-                parser.parsePageOrderingRule("25|26"));
-    }
-
-    @Test
-    public void parseThrowsOnEmptyPagesToProduce() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePagesToProduce(""));
-    }
-
-    @Test
-    public void parseThrowsOnMissingNumbersPagesToProduce() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> parser.parsePagesToProduce(","));
-    }
-
-    @Test
-    public void parseThrowsOnMissingFirstNumberPagesToProduce() {
-        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePagesToProduce(",26"));
-    }
-
-    @Test
-    public void parseThrowsOnNonNumbersPagesToProduce() {
-        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePagesToProduce("AB,CD"));
-    }
-
-    @Test
-    public void parseThrowsOnTwoNumbersIncludingSpacesPagesToProduce() {
-        Assertions.assertThrows(NumberFormatException.class, () -> parser.parsePagesToProduce("25, 26"));
-    }
-
-    @Test
-    public void parseSingleNumberPagesToProduce() {
-        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(25)),
-                parser.parsePagesToProduce("25"));
-    }
-
-    @Test
-    public void parseTwoNumbersPagesToProduce() {
-        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(25, 26)),
-                parser.parsePagesToProduce("25,26"));
-    }
-
-    @Test
-    public void parseOmitsEmptyNumberPagesToProduceAtTheEnd() {
-        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(25, 26)),
-                parser.parsePagesToProduce("25,26,"));
-    }
-
-    @Test
-    public void parseTenNumbersPagesToProduce() {
-        assertEquals(new ManualPrintingInstructions.PagesToProduce(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)),
-                parser.parsePagesToProduce("1,2,3,4,5,6,7,8,9,10"));
-    }
+    // TODO add test for real input file
 
     @AfterEach
     public void tearDown() {
