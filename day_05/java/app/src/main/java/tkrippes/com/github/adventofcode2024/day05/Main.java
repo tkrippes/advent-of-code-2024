171622ca2bebcc07262c89fde339e2b1a21a1b2c
tkrippes <tkrippes@live.de>
2024-12-11 22:17:40 +0100

    Add part 2 of day 5 to main
    add result to file

--- day_05/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day05/Main.java
+++ day_05/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day05/Main.java
@@ -2,16 +2,18 @@ package tkrippes.com.github.adventofcode2024.day05;
 
 import tkrippes.com.github.adventofcode2024.day05.parser.ManualPrintingInstructionsParser;
 import tkrippes.com.github.adventofcode2024.day05.solver.CorrectlyOrderedUpdatesSolver;
+import tkrippes.com.github.adventofcode2024.day05.solver.IncorrectlyOrderedUpdatesSolver;
 import tkrippes.com.github.adventofcode2024.day05.solver.ManualPrintingInstructionsSolver;
 
 public class Main {
     private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println("Result of part 1: " + part1(new CorrectlyOrderedUpdatesSolver()));
+        System.out.println("Result of part 1: " + commonPart(new CorrectlyOrderedUpdatesSolver()));
+        System.out.println("Result of part 2: " + commonPart(new IncorrectlyOrderedUpdatesSolver()));
     }
 
-    private static int part1(ManualPrintingInstructionsSolver solver) {
+    private static int commonPart(ManualPrintingInstructionsSolver solver) {
         ManualPrintingInstructionsParser parser = new ManualPrintingInstructionsParser();
         ManualPrintingInstructions instructions;
         try {
