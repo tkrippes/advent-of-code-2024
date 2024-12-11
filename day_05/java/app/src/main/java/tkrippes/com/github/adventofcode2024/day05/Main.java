0b96c3a0982ad798e6b1077ebfab324883f6c2bc
tkrippes <tkrippes@live.de>
2024-12-11 20:47:49 +0100

    Add main for part 1
    add result of part 1 of day 5 to file
    remove unneeded main test

--- day_05/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day05/Main.java
+++ day_05/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day05/Main.java
@@ -1,11 +1,36 @@
 package tkrippes.com.github.adventofcode2024.day05;
 
+import tkrippes.com.github.adventofcode2024.day05.parser.ManualPrintingInstructionsParser;
+import tkrippes.com.github.adventofcode2024.day05.solver.ManualPrintingInstructionsSolver;
+
 public class Main {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        System.out.println("Result of part 1: " + part1());
+    }
+
+    private static int part1() {
+        ManualPrintingInstructionsParser parser = new ManualPrintingInstructionsParser();
+        ManualPrintingInstructions instructions;
+        try {
+            instructions = parser.parse(inputFileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        ManualPrintingInstructionsSolver solver = new ManualPrintingInstructionsSolver();
+        int sumOfMiddlePageNumberOfCorrectlyOrderedUpdates;
+        try {
+            sumOfMiddlePageNumberOfCorrectlyOrderedUpdates = solver.solve(instructions);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        return sumOfMiddlePageNumberOfCorrectlyOrderedUpdates;
     }
 }
