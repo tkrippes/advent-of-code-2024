14fbb7050e9410d8f11ddd5948aae288826d0766
tkrippes <tkrippes@live.de>
2024-12-12 21:42:29 +0100

    Add solve to distinct positions solver
    add test

--- day_06/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day06/solver/DistinctPositionsSolver.java
+++ day_06/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day06/solver/DistinctPositionsSolver.java
@@ -1,4 +1,26 @@
 package tkrippes.com.github.adventofcode2024.day06.solver;
 
+import tkrippes.com.github.adventofcode2024.day06.map.LabMap;
+import tkrippes.com.github.adventofcode2024.day06.map.Position;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class DistinctPositionsSolver {
+    public int solve(LabMap map) {
+        Set<Position> distinctPositions = new HashSet<>();
+        distinctPositions.add(map.guard().getPosition());
+
+        do {
+            if (map.obstacleMap().get(map.guard().getNextPosition())) {
+                map.guard().turnRight();
+            } else {
+                map.guard().move();
+            }
+
+            distinctPositions.add(map.guard().getPosition());
+        } while (map.obstacleMap().containsKey(map.guard().getNextPosition()));
+
+        return distinctPositions.size();
+    }
 }
