75a982b2d12eb58375da28136d7630a9ecbcb3f0
Tom Krippes <tom.krippes@weinig.com>
2024-12-12 10:43:26 +0100

    Add helper classes position and entities

---
+++ day_06/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day06/Entity.java
@@ -0,0 +1,4 @@
+package tkrippes.com.github.adventofcode2024.day06;
+
+public interface Entity {
+}
---
+++ day_06/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day06/entity/Guard.java
@@ -0,0 +1,6 @@
+package tkrippes.com.github.adventofcode2024.day06.entity;
+
+import tkrippes.com.github.adventofcode2024.day06.Entity;
+
+public class Guard implements Entity {
+}
