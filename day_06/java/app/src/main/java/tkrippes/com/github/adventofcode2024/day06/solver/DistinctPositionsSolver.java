19552f5b3c333e91614394cd1a1693539531123f
Tom Krippes <tom.krippes@weinig.com>
2024-12-12 14:29:02 +0100

    Add distinct positions solver
    add test setup

---
+++ day_06/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day06/solver/DistinctPositionsSolver.java
@@ -0,0 +1,4 @@
+package tkrippes.com.github.adventofcode2024.day06.solver;
+
+public class DistinctPositionsSolver {
+}
---
+++ day_06/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day06/solver/DistinctPositionsSolverTest.java
@@ -0,0 +1,17 @@
+package tkrippes.com.github.adventofcode2024.day06.solver;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class DistinctPositionsSolverTest {
+    private DistinctPositionsSolver solver;
+
+    @BeforeEach
+    void setUp() {
+        solver = new DistinctPositionsSolver();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
