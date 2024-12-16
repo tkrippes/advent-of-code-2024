f5c2e9e4acfa20f0d431195da27add96c7123f8a
tkrippes <tkrippes@live.de>
2024-12-16 22:18:55 +0100

    Add main for day 7 java
    add result for day 7 part 1
    remove unneeded main test file

--- day_07/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day07/Main.java
+++ day_07/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day07/Main.java
@@ -1,11 +1,28 @@
 package tkrippes.com.github.adventofcode2024.day07;
 
+import tkrippes.com.github.adventofcode2024.day07.parser.EquationsParser;
+import tkrippes.com.github.adventofcode2024.day07.solver.TotalCalibrationResultSolver;
+
+import java.util.List;
+
 public class Main {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        System.out.println("Result of day 7 part 1: " + resultPart1());
+    }
+
+    private static long resultPart1() {
+        EquationsParser parser = new EquationsParser();
+        List<Equation> equations;
+        try {
+            equations = parser.parse(inputFileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        return new TotalCalibrationResultSolver().solve(equations);
     }
 }
---
+++ day_07/results.md
@@ -0,0 +1,5 @@
+# Results of Day 7
+
+## Part 1
+
+4998764814652
