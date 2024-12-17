df97ee59e4d0f8a8560637fe07466702449b11cf
tkrippes <tkrippes@live.de>
2024-12-17 21:12:34 +0100

    Add gradle project for day 8 java

---
+++ day_08/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day08/MainTest.java
@@ -0,0 +1,13 @@
+package tkrippes.com.github.adventofcode2024.day08;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void appHasAGreeting() {
+        Main classUnderTest = new Main();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+}
