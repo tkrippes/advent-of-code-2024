4751605ed321414dacca5e5b272d52e16cc39c0c
Tom Krippes <tom.krippes@weinig.com>
2024-12-18 13:49:54 +0100

    Add main for part 1 of day 8
    remove unneeded test
    add result for part 1 of day 8 to result file

--- day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/Main.java
+++ day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/Main.java
@@ -1,11 +1,25 @@
 package tkrippes.com.github.adventofcode2024.day08;
 
+import tkrippes.com.github.adventofcode2024.day08.parser.AntennaMapParser;
+import tkrippes.com.github.adventofcode2024.day08.solver.AntinodesSolver;
+
 public class Main {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+    private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        System.out.println("Result of day 7 part 1: " + resultPart1());
+    }
+
+    private static int resultPart1() {
+        AntennaMap map;
+        try {
+            map = AntennaMapParser.parse(inputFileName);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return 0;
+        }
+
+        return AntinodesSolver.solve(map);
     }
 }
---
+++ day_08/results.md
@@ -0,0 +1,5 @@
+# Results of Day 8
+
+## Part 1
+
+394
