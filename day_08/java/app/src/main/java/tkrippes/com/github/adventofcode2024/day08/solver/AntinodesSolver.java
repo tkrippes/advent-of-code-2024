60279c168739b6d564fddbe9596f3e682eb30123
Tom Krippes <tom.krippes@weinig.com>
2024-12-18 12:03:38 +0100

    Implement getAntennaPositionsPairCombinations in solver
    add tests

--- day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/solver/AntinodesSolver.java
+++ day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/solver/AntinodesSolver.java
@@ -3,9 +3,22 @@ package tkrippes.com.github.adventofcode2024.day08.solver;
 import tkrippes.com.github.adventofcode2024.day08.Position;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class AntinodesSolver {
+    static Set<Set<Position>> getPositionsPairCombinations(Set<Position> positions) {
+        List<Position> positionsList = List.copyOf(positions);
+        Set<Set<Position>> positionsPairCombinations = new HashSet<>();
+        for (int i = 0; i < positionsList.size(); i++) {
+            for (int j = i + 1; j < positionsList.size(); j++) {
+                positionsPairCombinations.add(Set.of(positionsList.get(i), positionsList.get(j)));
+            }
+        }
+
+        return positionsPairCombinations;
+    }
+
     static Set<Position> getAntinodesPositions(Position antennaPosition1, Position antennaPosition2) {
         Position vectorFromAntenna1ToAntenna2 = antennaPosition2.subtract(antennaPosition1);
 
