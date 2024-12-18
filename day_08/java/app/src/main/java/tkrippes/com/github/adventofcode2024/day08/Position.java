0f9fc6848af4cef267563577cecd9b5555974583
Tom Krippes <tom.krippes@weinig.com>
2024-12-18 08:53:15 +0100

    Add add and substract to Position
    rename x and y to row and column

--- day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/Position.java
+++ day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/Position.java
@@ -1,4 +1,11 @@
 package tkrippes.com.github.adventofcode2024.day08;
 
-public record Position(int x, int y) {
+public record Position(int row, int column) {
+    public Position add(Position otherPosition) {
+        return new Position(row + otherPosition.row(), column + otherPosition.column());
+    }
+
+    public Position subtract(Position otherPosition) {
+        return new Position(row - otherPosition.row(), column - otherPosition.column());
+    }
 }
