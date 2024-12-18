06b1481528fe55037e01ab81530377041a2e8d93
Tom Krippes <tom.krippes@weinig.com>
2024-12-18 15:24:35 +0100

    Add part 2 of day 8 to main
    add result to results

--- day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/Main.java
+++ day_08/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day08/Main.java
@@ -1,16 +1,20 @@
 package tkrippes.com.github.adventofcode2024.day08;
 
 import tkrippes.com.github.adventofcode2024.day08.parser.AntennaMapParser;
+import tkrippes.com.github.adventofcode2024.day08.solver.AntinodesResonantHarmonicsSolver;
 import tkrippes.com.github.adventofcode2024.day08.solver.AntinodesSolver;
+
+import java.util.function.Function;
 
 public class Main {
     private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println("Result of day 7 part 1: " + resultPart1());
+        System.out.println("Result of day 7 part 1: " + result(AntinodesSolver::solve));
+        System.out.println("Result of day 7 part 2: " + result(AntinodesResonantHarmonicsSolver::solve));
     }
 
-    private static int resultPart1() {
+    private static int result(Function<AntennaMap, Integer> solve) {
         AntennaMap map;
         try {
             map = AntennaMapParser.parse(inputFileName);
@@ -20,6 +24,6 @@ public class Main {
             return 0;
         }
 
-        return AntinodesSolver.solve(map);
+        return solve.apply(map);
     }
 }
