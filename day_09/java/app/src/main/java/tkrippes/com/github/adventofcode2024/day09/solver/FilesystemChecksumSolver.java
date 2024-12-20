1b96b996b4a18c8d3b16876635eaca808331cd96
Tom Krippes <tom.krippes@weinig.com>
2024-12-20 13:18:18 +0100

    Add getCompactedFilesystem to solver
    add test

--- day_09/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolver.java
+++ day_09/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolver.java
@@ -1,8 +1,36 @@
 package tkrippes.com.github.adventofcode2024.day09.solver;
 
+import tkrippes.com.github.adventofcode2024.day09.DiskMap;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FilesystemChecksumSolver {
+    static String getCompactedFilesystem(DiskMap map) {
+        String filesystem = map.toString();
+        int frontIndex = 0;
+        int backIndex = filesystem.length() - 1;
+
+        while (frontIndex < backIndex) {
+            if (filesystem.charAt(frontIndex) != '.') {
+                frontIndex++;
+            } else if (filesystem.charAt(backIndex) == '.') {
+                backIndex--;
+            } else {
+                filesystem = swapCharacters(filesystem, frontIndex, backIndex);
+            }
+        }
+
+        return filesystem;
+    }
+
+    private static String swapCharacters(String filesystem, int characterIndex1, int characterIndex2) {
+        char temp = filesystem.charAt(characterIndex1);
+        filesystem =
+                filesystem.substring(0, characterIndex1) + filesystem.charAt(characterIndex2) + filesystem.substring(characterIndex1 + 1);
+        filesystem = filesystem.substring(0, characterIndex2) + temp + filesystem.substring(characterIndex2 + 1);
+        return filesystem;
+    }
+
     static int calculateChecksum(String compactedFilesystem) {
         AtomicInteger index = new AtomicInteger();
 
--- day_09/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolverTest.java
+++ day_09/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolverTest.java
@@ -1,14 +1,40 @@
 package tkrippes.com.github.adventofcode2024.day09.solver;
 
+import tkrippes.com.github.adventofcode2024.day09.DiskMap;
+import tkrippes.com.github.adventofcode2024.day09.disk.File;
+import tkrippes.com.github.adventofcode2024.day09.disk.FileFreeSpaceBlock;
+import tkrippes.com.github.adventofcode2024.day09.disk.FreeSpace;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FilesystemChecksumSolverTest {
     @BeforeEach
     public void setUp() {
+    }
+
+    @Test
+    public void getCompactedFilesystemForTestInputShouldReturnCorrectFilesystem() {
+        DiskMap map = new DiskMap(List.of(
+                new FileFreeSpaceBlock(new File(2, 0), Optional.of(new FreeSpace(3))),
+                new FileFreeSpaceBlock(new File(3, 1), Optional.of(new FreeSpace(3))),
+                new FileFreeSpaceBlock(new File(1, 2), Optional.of(new FreeSpace(3))),
+                new FileFreeSpaceBlock(new File(3, 3), Optional.of(new FreeSpace(1))),
+                new FileFreeSpaceBlock(new File(2, 4), Optional.of(new FreeSpace(1))),
+                new FileFreeSpaceBlock(new File(4, 5), Optional.of(new FreeSpace(1))),
+                new FileFreeSpaceBlock(new File(4, 6), Optional.of(new FreeSpace(1))),
+                new FileFreeSpaceBlock(new File(3, 7), Optional.of(new FreeSpace(1))),
+                new FileFreeSpaceBlock(new File(4, 8), Optional.of(new FreeSpace(0))),
+                new FileFreeSpaceBlock(new File(2, 9), Optional.empty())));
+
+        assertEquals("0099811188827773336446555566..............",
+                FilesystemChecksumSolver.getCompactedFilesystem(map));
     }
 
     @Test
