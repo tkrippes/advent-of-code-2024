baec8fe7981938ad50849d939cee20afdbb6c79c
tkrippes <tkrippes@live.de>
2024-12-20 22:15:06 +0100

    Fix solver
    moving single characters does only work for single digit numbers
    creating List<Integer> used for calculation
    adapt tests
    add correct result for part 1 of day 9

--- day_09/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolver.java
+++ day_09/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolver.java
@@ -1,46 +1,56 @@
 package tkrippes.com.github.adventofcode2024.day09.solver;
 
 import tkrippes.com.github.adventofcode2024.day09.DiskMap;
+import tkrippes.com.github.adventofcode2024.day09.disk.File;
+import tkrippes.com.github.adventofcode2024.day09.disk.FreeSpace;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class FilesystemChecksumSolver {
     public static long solve(DiskMap map) {
-        return calculateChecksum(getCompactedFilesystem(map));
+        return calculateChecksum(getCompactedFilesystemLayout(getFilesystemLayout(map)));
     }
 
-    static String getCompactedFilesystem(DiskMap map) {
-        String filesystem = map.toString();
+    static List<Integer> getFilesystemLayout(DiskMap map) {
+        return map.filesystem().stream()
+                .flatMap(entity -> {
+                    if (entity instanceof File(int size, int id)) {
+                        return java.util.Collections.nCopies(size, id).stream();
+                    } else if (entity instanceof FreeSpace(int size)) {
+                        return java.util.Collections.nCopies(size, -1).stream();
+                    }
+
+                    return java.util.stream.Stream.empty();
+                }).toList();
+    }
+
+    static List<Integer> getCompactedFilesystemLayout(List<Integer> filesystemLayout) {
         int frontIndex = 0;
-        int backIndex = filesystem.length() - 1;
+        int backIndex = filesystemLayout.size() - 1;
+        List<Integer> compactedFilesystemLayout = new ArrayList<>(filesystemLayout);
 
         while (frontIndex < backIndex) {
-            if (filesystem.charAt(frontIndex) != '.') {
+            if (compactedFilesystemLayout.get(frontIndex) != -1) {
                 frontIndex++;
-            } else if (filesystem.charAt(backIndex) == '.') {
+            } else if (compactedFilesystemLayout.get(backIndex) == -1) {
                 backIndex--;
             } else {
-                filesystem = swapCharacters(filesystem, frontIndex, backIndex);
+                Collections.swap(compactedFilesystemLayout, frontIndex, backIndex);
             }
         }
 
-        return filesystem;
+        return compactedFilesystemLayout;
     }
 
-    private static String swapCharacters(String filesystem, int characterIndex1, int characterIndex2) {
-        char temp = filesystem.charAt(characterIndex1);
-        filesystem =
-                filesystem.substring(0, characterIndex1) + filesystem.charAt(characterIndex2) + filesystem.substring(characterIndex1 + 1);
-        filesystem = filesystem.substring(0, characterIndex2) + temp + filesystem.substring(characterIndex2 + 1);
-        return filesystem;
-    }
-
-    static long calculateChecksum(String compactedFilesystem) {
+    static long calculateChecksum(List<Integer> compactedFilesystemLayout) {
         AtomicInteger index = new AtomicInteger();
 
-        return compactedFilesystem.chars()
-                .takeWhile(ch -> ch != '.')
-                .mapToLong(Character::getNumericValue)
+        return compactedFilesystemLayout.stream()
+                .takeWhile(i -> i != -1)
+                .mapToLong(Integer::longValue)
                 .reduce(0L, (sum, value) -> sum + index.getAndIncrement() * value);
     }
 }
--- day_09/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolverTest.java
+++ day_09/java/app/src/test/java/tkrippes/com/github/adventofcode2024/day09/solver/FilesystemChecksumSolverTest.java
@@ -30,7 +30,7 @@ public class FilesystemChecksumSolverTest {
     }
 
     @Test
-    public void getCompactedFilesystemForTestInputShouldReturnCorrectFilesystem() {
+    public void getFilesystemLayoutForTestInputShouldReturnCorrectFilesystemLayout() {
         DiskMap map = new DiskMap(List.of(
                 new File(2, 0), new FreeSpace(3), new File(3, 1), new FreeSpace(3),
                 new File(1, 2), new FreeSpace(3), new File(3, 3), new FreeSpace(1),
@@ -38,13 +38,42 @@ public class FilesystemChecksumSolverTest {
                 new File(4, 6), new FreeSpace(1), new File(3, 7), new FreeSpace(1),
                 new File(4, 8), new FreeSpace(0), new File(2, 9)));
 
-        assertEquals("0099811188827773336446555566..............",
-                FilesystemChecksumSolver.getCompactedFilesystem(map));
+        List<Integer> expectedLayout = List.of(0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1, 3, 3, 3, -1, 4,
+                4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9);
+
+        assertEquals(expectedLayout, FilesystemChecksumSolver.getFilesystemLayout(map));
+    }
+
+    @Test
+    public void getCompactedFilesystemLayoutForTestInputShouldReturnCorrectFilesystem() {
+        List<Integer> layout = List.of(0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1,
+                3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9);
+
+        List<Integer> expectedCompactedLayout = List.of(0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4,
+                4, 6, 5, 5, 5, 5, 6, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+
+        assertEquals(expectedCompactedLayout,
+                FilesystemChecksumSolver.getCompactedFilesystemLayout(layout));
+    }
+
+    @Test
+    public void getCompactedFilesystemLayoutForTestInputPlusTwoDigitsFileIdShouldReturnCorrectFilesystem() {
+        List<Integer> layout = List.of(0, 0, -1, -1, -1, 1, 1, 1, -1, -1, -1, 2, -1, -1, -1,
+                3, 3, 3, -1, 4, 4, -1, 5, 5, 5, 5, -1, 6, 6, 6, 6, -1, 7, 7, 7, -1, 8, 8, 8, 8, 9, 9, -1, 10);
+
+        List<Integer> expectedCompactedLayout = List.of(0, 0, 10, 9, 9, 1, 1, 1, 8, 8, 8, 2, 8, 7, 7, 3, 3, 3, 7,
+                4, 4, 6, 5, 5, 5, 5, 6, 6, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+
+        assertEquals(expectedCompactedLayout,
+                FilesystemChecksumSolver.getCompactedFilesystemLayout(layout));
     }
 
     @Test
     public void calculateChecksumForTestInputShouldReturnCorrectChecksum() {
-        assertEquals(1928, FilesystemChecksumSolver.calculateChecksum("0099811188827773336446555566.............."));
+        List<Integer> compactedLayout = List.of(0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4,
+                4, 6, 5, 5, 5, 5, 6, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1);
+
+        assertEquals(1928, FilesystemChecksumSolver.calculateChecksum(compactedLayout));
     }
 
     @AfterEach
