c9deb17f66aa7c1c654bd13beaa4b260150d3d54
tkrippes <tkrippes@live.de>
2024-12-21 22:08:13 +0100

    Add solveV2 to main
    add result hint
    infinite loop to fix

--- day_09/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day09/Main.java
+++ day_09/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day09/Main.java
@@ -3,15 +3,17 @@ package tkrippes.com.github.adventofcode2024.day09;
 import tkrippes.com.github.adventofcode2024.day09.parser.DiskMapParser;
 import tkrippes.com.github.adventofcode2024.day09.solver.FilesystemChecksumSolver;
 
+import java.util.function.Function;
+
 public class Main {
     private static final String inputFileName = "../../input/input.txt";
 
-    // TODO try to use long everywhere
     public static void main(String[] args) {
-        System.out.println("Result of day 9 part 1: " + resultPart1());
+        System.out.println("Result of day 9 part 1: " + result(FilesystemChecksumSolver::solve));
+        System.out.println("Result of day 9 part 2: " + result(FilesystemChecksumSolver::solveV2));
     }
 
-    private static long resultPart1() {
+    private static long result(Function<DiskMap, Long> solve) {
         DiskMap map;
         try {
             map = DiskMapParser.parse(inputFileName);
@@ -21,6 +23,6 @@ public class Main {
             return 0;
         }
 
-        return FilesystemChecksumSolver.solve(map);
+        return solve.apply(map);
     }
 }
