7601a15f7278e9eb5697a7a9ff6aa9f2adf9e87b
tkrippes <tkrippes@live.de>
2024-12-23 21:42:03 +0100

    Add width, height and equals to topographic map
    rename map to height map

--- day_10/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day10/Main/TopographicMap.java
+++ day_10/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day10/Main/TopographicMap.java
@@ -7,23 +7,47 @@ import java.util.stream.Collectors;
 public class TopographicMap {
     private final int trailheadValue = 0;
     private final int trailEndValue = 9;
-    private final Map<Position, Integer> map;
 
-    public TopographicMap(Map<Position, Integer> map) {
-        this.map = map;
+    private final Map<Position, Integer> heightMap;
+    private final int mapWidth;
+    private final int mapHeight;
+
+    public TopographicMap(Map<Position, Integer> heightMap, int mapWidth, int mapHeight) {
+        this.heightMap = heightMap;
+        this.mapWidth = mapWidth;
+        this.mapHeight = mapHeight;
     }
 
     public Set<Position> getTrailheadPositions() {
-        return map.entrySet().stream()
+        return heightMap.entrySet().stream()
                 .filter(e -> e.getValue() == trailheadValue)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toSet());
     }
 
     public Set<Position> getTrailEndPositions() {
-        return map.entrySet().stream()
+        return heightMap.entrySet().stream()
                 .filter(e -> e.getValue() == trailEndValue)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        TopographicMap other = (TopographicMap) obj;
+
+        return heightMap.equals(other.heightMap) && mapWidth == other.mapWidth && mapHeight == other.mapHeight;
     }
 }
