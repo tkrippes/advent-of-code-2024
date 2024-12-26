8dcff5f9c1160aed1247744279ab0dd7270c9aae
tkrippes <tkrippes@live.de>
2024-12-26 21:02:06 +0100

    Add part 2 of day 10 to main
    add result to results file

--- day_10/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day10/Main.java
+++ day_10/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day10/Main.java
@@ -1,16 +1,20 @@
 package tkrippes.com.github.adventofcode2024.day10;
 
 import tkrippes.com.github.adventofcode2024.day10.parser.TopographicMapParser;
+import tkrippes.com.github.adventofcode2024.day10.solver.TrailheadRatingSolver;
 import tkrippes.com.github.adventofcode2024.day10.solver.TrailheadScoreSolver;
+
+import java.util.function.Function;
 
 public class Main {
     private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println("Result of day 10 part 1: " + resultPart1());
+        System.out.println("Result of day 10 part 1: " + result(TrailheadScoreSolver::solve));
+        System.out.println("Result of day 10 part 2: " + result(TrailheadRatingSolver::solve));
     }
 
-    private static int resultPart1() {
+    private static int result(Function<TopographicMap, Integer> solve) {
         TopographicMap map;
         try {
             map = TopographicMapParser.parse(inputFileName);
@@ -20,6 +24,6 @@ public class Main {
             return 0;
         }
 
-        return TrailheadScoreSolver.solve(map);
+        return solve.apply(map);
     }
 }
