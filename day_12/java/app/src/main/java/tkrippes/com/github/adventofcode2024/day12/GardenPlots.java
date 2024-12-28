70cb2da3c4ebb08438356d92f7a1d84f0cd89700
tkrippes <tkrippes@live.de>
2024-12-28 22:35:44 +0100

    Adjust PriceSolver to account for regions
    some renaming on GardenPlots
    add convenience overwrite for toString for Position

--- day_12/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day12/GardenPlots.java
+++ day_12/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day12/GardenPlots.java
@@ -2,5 +2,5 @@ package tkrippes.com.github.adventofcode2024.day12;
 
 import java.util.Map;
 
-public record GardenPlots(Map<Position, Character> gardenPlot) {
+public record GardenPlots(Map<Position, Character> gardenPlots) {
 }
--- day_12/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day12/solver/PriceSolver.java
+++ day_12/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day12/solver/PriceSolver.java
@@ -3,59 +3,99 @@ package tkrippes.com.github.adventofcode2024.day12.solver;
 import tkrippes.com.github.adventofcode2024.day12.GardenPlots;
 import tkrippes.com.github.adventofcode2024.day12.Position;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class PriceSolver {
     public static long solve(GardenPlots plots) {
-        Map<Character, Long> areas = getAreas(plots);
-        Map<Character, Long> perimeters = getPerimeters(plots);
+        List<List<Position>> regions = getRegions(plots.gardenPlots());
+        List<Long> areas = getAreas(regions);
+        List<Long> perimeters = getPerimeters(regions);
 
-        long price = 0;
-        for (Character plantType : areas.keySet()) {
-            price += areas.get(plantType) * perimeters.get(plantType);
-        }
-
-        return price;
+        return IntStream.range(0, regions.size())
+                .mapToLong(i -> areas.get(i) * perimeters.get(i))
+                .sum();
     }
 
-    private static Map<Character, Long> getAreas(GardenPlots plots) {
-        Map<Character, Long> areas = new HashMap<>();
-        for (Map.Entry<Position, Character> plot : plots.gardenPlot().entrySet()) {
-            areas.putIfAbsent(plot.getValue(), 0L);
-            areas.put(plot.getValue(), areas.get(plot.getValue()) + 1);
+    private static List<List<Position>> getRegions(Map<Position, Character> plots) {
+        // TODO refactor
+        List<List<Position>> regions = new ArrayList<>();
+        Map<Position, Boolean> visitedPositions = new HashMap<>();
+        for (Position position : plots.keySet()) {
+            visitedPositions.put(position, false);
         }
 
-        return areas;
-    }
+        for (Map.Entry<Position, Character> plot : plots.entrySet()) {
+            if (!visitedPositions.get(plot.getKey())) {
+                List<Position> region = new ArrayList<>();
+                List<Map.Entry<Position, Character>> queue = new ArrayList<>();
+                queue.add(plot);
+                visitedPositions.put(plot.getKey(), true);
+                region.add(plot.getKey());
 
-    private static Map<Character, Long> getPerimeters(GardenPlots plots) {
-        Map<Character, Long> perimeters = new HashMap<>();
-        for (Map.Entry<Position, Character> plot : plots.gardenPlot().entrySet()) {
-            perimeters.putIfAbsent(plot.getValue(), 0L);
-            switch (getNumberOfNeighboursWithSamePlantType(plot, plots)) {
-                case 0 -> perimeters.put(plot.getValue(), perimeters.get(plot.getValue()) + 4);
-                case 1 -> perimeters.put(plot.getValue(), perimeters.get(plot.getValue()) + 3);
-                case 2 -> perimeters.put(plot.getValue(), perimeters.get(plot.getValue()) + 2);
-                case 3 -> perimeters.put(plot.getValue(), perimeters.get(plot.getValue()) + 1);
+                while (!queue.isEmpty()) {
+                    Map.Entry<Position, Character> currentPlot = queue.removeFirst();
+                    for (Map.Entry<Position, Character> neighbourPlot :
+                            getNeighbourPlotsWithSamePlantType(currentPlot.getKey(),
+                                    currentPlot.getValue(), plots)) {
+                        if (!visitedPositions.get(neighbourPlot.getKey())) {
+                            queue.add(neighbourPlot);
+                            visitedPositions.put(neighbourPlot.getKey(), true);
+                            region.add(neighbourPlot.getKey());
+                        }
+                    }
+                }
+
+                regions.add(region);
             }
         }
 
-        return perimeters;
+        return regions;
     }
 
-    private static int getNumberOfNeighboursWithSamePlantType(Map.Entry<Position, Character> plot, GardenPlots plots) {
-        int numberOfNeighbours = 0;
-        Position position = plot.getKey();
-        Character plantType = plot.getValue();
-        
+    private static List<Map.Entry<Position, Character>> getNeighbourPlotsWithSamePlantType(Position position,
+                                                                                           Character plantType,
+                                                                                           Map<Position, Character> plots) {
+        List<Map.Entry<Position, Character>> neighbourPlots = new ArrayList<>();
         for (Position neighbourPosition : List.of(
                 new Position(position.row() - 1, position.column()),
                 new Position(position.row() + 1, position.column()),
                 new Position(position.row(), position.column() - 1),
                 new Position(position.row(), position.column() + 1))) {
-            if (plots.gardenPlot().get(neighbourPosition) == plantType) {
+            if (plots.get(neighbourPosition) == plantType) {
+                neighbourPlots.add(Map.entry(neighbourPosition, plots.get(neighbourPosition)));
+            }
+        }
+
+        return neighbourPlots;
+    }
+
+    private static List<Long> getAreas(List<List<Position>> regions) {
+        return regions.stream().mapToLong(List::size).boxed().toList();
+    }
+
+    private static List<Long> getPerimeters(List<List<Position>> regions) {
+        return regions.stream()
+                .map(region -> region.stream()
+                        .mapToLong(position -> switch (getNumberOfNeighboursInRegion(position, region)) {
+                            case 0 -> 4;
+                            case 1 -> 3;
+                            case 2 -> 2;
+                            case 3 -> 1;
+                            default -> 0;
+                        }).sum()
+                ).toList();
+    }
+
+    private static int getNumberOfNeighboursInRegion(Position position, List<Position> region) {
+        int numberOfNeighbours = 0;
+
+        for (Position neighbourPosition : List.of(
+                new Position(position.row() - 1, position.column()),
+                new Position(position.row() + 1, position.column()),
+                new Position(position.row(), position.column() - 1),
+                new Position(position.row(), position.column() + 1))) {
+            if (region.contains(neighbourPosition)) {
                 numberOfNeighbours++;
             }
         }
