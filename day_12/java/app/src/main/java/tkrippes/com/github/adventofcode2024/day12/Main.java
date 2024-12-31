bb53ca9162b31d7e4000f7243dbc1e6084a24dd5
tkrippes <tkrippes@live.de>
2024-12-31 15:44:23 +0100

    Add solution of part 2 of day 12 to main

--- day_12/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day12/Main.java
+++ day_12/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day12/Main.java
@@ -3,14 +3,17 @@ package tkrippes.com.github.adventofcode2024.day12;
 import tkrippes.com.github.adventofcode2024.day12.parser.GardenPlotsParser;
 import tkrippes.com.github.adventofcode2024.day12.solver.PriceSolver;
 
+import java.util.function.Function;
+
 public class Main {
     private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println("Result of day 12 part 1: " + resultPart1());
+        System.out.println("Result of day 12 part 1: " + result(PriceSolver::solve));
+        System.out.println("Result of day 12 part 2: " + result(PriceSolver::solveV2));
     }
 
-    private static long resultPart1() {
+    private static long result(Function<GardenPlots, Long> solve) {
         GardenPlots plots;
         try {
             plots = GardenPlotsParser.parse(inputFileName);
@@ -20,6 +23,6 @@ public class Main {
             return 0;
         }
 
-        return PriceSolver.solve(plots);
+        return solve.apply(plots);
     }
 }
