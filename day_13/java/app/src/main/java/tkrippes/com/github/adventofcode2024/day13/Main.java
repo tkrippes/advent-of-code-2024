d43e26eb092e6aefea8ee4c4d0cd8140bc01ba1e
tkrippes <tkrippes@live.de>
2025-01-02 21:25:43 +0100

    Add solution for part 2 of day 13 to main
    add result to results file

--- day_13/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day13/Main.java
+++ day_13/java/app/src/main/java/tkrippes/com/github/adventofcode2024/day13/Main.java
@@ -1,18 +1,21 @@
 package tkrippes.com.github.adventofcode2024.day13;
 
 import tkrippes.com.github.adventofcode2024.day13.parser.MachineConfigurationsParser;
-import tkrippes.com.github.adventofcode2024.day13.solver.TokenOptimizerSolver;
+import tkrippes.com.github.adventofcode2024.day13.solver.TokenOptimizerSolverV1;
+import tkrippes.com.github.adventofcode2024.day13.solver.TokenOptimizerSolverV2;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class Main {
     private static final String inputFileName = "../../input/input.txt";
 
     public static void main(String[] args) {
-        System.out.println("Result of day 12 part 1: " + resultPart1());
+        System.out.println("Result of day 12 part 1: " + result(TokenOptimizerSolverV1::solve));
+        System.out.println("Result of day 12 part 2: " + result(TokenOptimizerSolverV2::solve));
     }
 
-    private static long resultPart1() {
+    private static long result(Function<List<MachineConfiguration>, Long> solve) {
         List<MachineConfiguration> machineConfigurations;
         try {
             machineConfigurations = MachineConfigurationsParser.parse(inputFileName);
@@ -22,6 +25,6 @@ public class Main {
             return 0;
         }
 
-        return TokenOptimizerSolver.solve(machineConfigurations);
+        return solve.apply(machineConfigurations);
     }
 }
